b9e5100f915c13b62a472dfeb9de05f00ab0daf6
Hugo Ferreira da Silva <hugo.silva@casaavenida.com.br>
2026-03-18 18:10:31 -0300

    Exercicio 9

---
+++ src/main/java/POO/exercicio9/Carro.java
@@ -0,0 +1,19 @@
+package POO.exercicio9;
+
+public class Carro extends Veiculo{
+
+    private int portas;
+
+    public Carro(String marca, String modelo, int ano, int portas){
+        super(marca, modelo, ano);
+        this.portas = portas;
+    }
+
+    public int getPortas(){
+        return portas;
+    }
+
+    public void setPortas(int portas){
+        this.portas = portas;
+    }
+}
